6238c964753d1b48496e7039f9afef3409102673
Your Name <you@example.com>
2025-03-05 05:41:53 +0200

    title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,27 +5,33 @@ export const routes: Routes = [
 
   {
     path: 'home',
-    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
+    title: 'Home',
   },
   {
     path: 'aboutus',
-    loadComponent: () => import('./aboutus/aboutus.component').then(m => m.AboutusComponent)
+    loadComponent: () => import('./aboutus/aboutus.component').then(m => m.AboutusComponent),
+    title: 'About-us',
   },
   {
     path: 'contactus',
-    loadComponent: () => import('./contactus/contactus.component').then(m => m.ContactusComponent)
+    loadComponent: () => import('./contactus/contactus.component').then(m => m.ContactusComponent),
+    title: 'Contact-us',
   },
   {
     path: 'services',
-    loadComponent: () => import('./services/services.component').then(m => m.ServicesComponent)
+    loadComponent: () => import('./services/services.component').then(m => m.ServicesComponent),
+    title: 'Services',
   },
   {
     path: 'strategy',
-    loadComponent: () => import('./strategy/strategy.component').then(m => m.StrategyComponent)
+    loadComponent: () => import('./strategy/strategy.component').then(m => m.StrategyComponent),
+    title: 'Strategy',
   },
 
   {
     path: '**',
-    loadComponent: () => import('./notfound/notfound.component').then(m => m.NotfoundComponent)
+    loadComponent: () => import('./notfound/notfound.component').then(m => m.NotfoundComponent),
+    title: 'Not Found',
   }
 ];
