dfd486425904b202cf9935620bac2366d1cfb671
Your Name <you@example.com>
2025-03-06 13:48:21 +0200

    link edit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
